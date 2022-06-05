0cb33352c269682d57d6f83a9824819c1dd1e023
Alex Robinson <girotobial@gmail.com>
2022-06-05 15:34:22 +0100

    🧪 add config default val test

---
+++ tests/test_config.py
@@ -0,0 +1,71 @@
+"""Tests for the new config framework"""
+
+from src import config
+from src.common import CoverZLConfig, ScreenRes
+
+
+class TestConfigClass:
+    def test_init_with_defaults(self):
+        cfg = config.Config()
+        assert cfg.verbosity == 1
+        assert cfg.cleaning_level == 1
+        assert cfg.overpass_server_choice == "random"
+        assert not cfg.skip_downloads
+        assert not cfg.skip_converts
+        assert cfg.max_convert_slots == 4
+        assert cfg.check_tms_response
+        assert cfg.http_timeout == 10
+        assert cfg.max_connect_retries == 5
+        assert cfg.max_baddata_retries == 5
+        assert cfg.ovl_exclude_pol == [0]
+        assert cfg.ovl_exclude_net == []
+        assert cfg.xplane_install_dir == "<X-Plane Top Level directory>"
+        assert cfg.custom_overlay_src == ""
+        assert cfg.apt_smoothing_pix == 8
+        assert cfg.road_level == 1
+        assert cfg.road_banking_limit == 0.5
+        assert cfg.lane_width == 5
+        assert cfg.max_levelled_segs == 100000
+        assert cfg.water_simplification == 0
+        assert cfg.min_area == 0.001
+        assert cfg.max_area == 200
+        assert cfg.clean_bad_geometries
+        assert cfg.mesh_zl == 19
+        assert cfg.curvature_tol == float(2)
+        assert cfg.apt_curv_tol == 0.5
+        assert cfg.apt_curv_ext == 0.5
+        assert cfg.coast_curv_tol == 1
+        assert cfg.coast_curv_ext == 0.5
+        assert cfg.limit_tris == 0
+        assert cfg.hmin == 0
+        assert cfg.min_angle == 10
+        assert cfg.sea_smoothing_mode == "zero"
+        assert cfg.water_smoothing == 10
+        assert cfg.iterate == 0
+        assert cfg.mask_zl == 14
+        assert cfg.masks_width == 100
+        assert cfg.masking_mode == "sand"
+        assert not cfg.use_masks_for_inland
+        assert not cfg.imprint_masks_to_dds
+        assert not cfg.masks_use_dem_too
+        assert cfg.masks_custom_extent == ""
+        assert cfg.default_website == ""
+        assert cfg.default_zl == 16
+        assert cfg.zone_list == []
+        assert cfg.cover_airports_with_highres == "False"
+        assert cfg.cover_extent == 1
+        assert cfg.cover_zl == 18
+        assert cfg.cover_screen_res == "HD_1080p"
+        assert cfg.cover_fov == 60.0
+        assert cfg.cover_fpa == 10
+        assert cfg.cover_greediness == 1
+        assert cfg.cover_greediness_threshold == 0.7
+        assert cfg.sea_texture_blur == 0
+        assert not cfg.add_low_res_sea_ovl
+        assert cfg.experimental_water == 0
+        assert cfg.ratio_water == 0.25
+        assert cfg.normal_map_strength == 1
+        assert cfg.terrain_casts_shadows
+        assert cfg.overlay_lod == 25000
+        assert cfg.custom_dem == ""
+        assert cfg.fill_nodata
